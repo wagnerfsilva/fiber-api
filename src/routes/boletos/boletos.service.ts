48f6d29e261a2a339802c9ce1679dfcddf540e51
IgorDevJS <igor_goncalves_ferrari@hotmail.com>
2023-07-31 09:04:04 -0300

    add TODO

--- src/routes/boletos/boletos.service.ts
+++ src/routes/boletos/boletos.service.ts
@@ -4,6 +4,7 @@ import { BoletosResponse } from './dtos/boletos-response.dto';
 @Injectable()
 export class BoletosService {
   async obtemBoletos(): Promise<BoletosResponse> {
+    // TODO: retornar dados da API do cliente
     return {
       boletos: [
         {

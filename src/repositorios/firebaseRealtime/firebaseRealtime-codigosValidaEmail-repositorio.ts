1a9b1951cf5fc58dfbc3304692d22ab4886d8cee
IgorDevJS <igor_goncalves_ferrari@hotmail.com>
2023-07-31 23:01:05 -0300

    trata cpf no repositorio codigosValidaEmail

--- src/repositorios/firebaseRealtime/firebaseRealtime-codigosValidaEmail-repositorio.ts
+++ src/repositorios/firebaseRealtime/firebaseRealtime-codigosValidaEmail-repositorio.ts
@@ -17,29 +17,33 @@ export class FirebaseRealtimeCodigosValidaEmailRepositorio
     private readonly firebaseRealtimeService: FirebaseRealtimeService,
   ) {}
 
+  private trataCPF(value: string) {
+    return value.replace(/\D/g, '');
+  }
+
   async findUnique(props: FindUniqueProps): Promise<CodigosValidaEmail | null> {
     return (
       await await this.firebaseRealtimeService.codigosValidaEmail
-        .child(props.where.cpf)
+        .child(this.trataCPF(props.where.cpf))
         .get()
     ).val() as CodigosValidaEmail | null;
   }
 
   async create(props: CreateProps): Promise<void> {
     await this.firebaseRealtimeService.codigosValidaEmail
-      .child(props.where.cpf)
+      .child(this.trataCPF(props.where.cpf))
       .set(props.data);
   }
 
   async update(props: UpdateProps): Promise<void> {
     await this.firebaseRealtimeService.codigosValidaEmail
-      .child(props.where.cpf)
+      .child(this.trataCPF(props.where.cpf))
       .update(props.data);
   }
 
   async delete(props: DeleteProps): Promise<void> {
     await this.firebaseRealtimeService.codigosValidaEmail
-      .child(props.where.cpf)
+      .child(this.trataCPF(props.where.cpf))
       .remove();
   }
 }
